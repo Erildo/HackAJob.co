98aac35ca9cf0aee67f7c66c3f29b4272ef88145
Erildo Shuli <Erildo@users.noreply.github.com>
2017-10-13 14:45:33 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,7 +1,7 @@
-
+// Levl 4 Longest Increasing sub
 public class Solution {
-
-	public static int run(int[] seaquence) {
+	
+public static int run(int[] seaquence) {
 		int[] max = new int[seaquence.length];
 		 for(int i=0; i<seaquence.length; i++){
 		        max[i]=1;
@@ -11,23 +11,12 @@ public class Solution {
 		            }
 		        }
 		    }
-		 int result = 0;
+		 int k = 0;
 		    for(int i=0; i<max.length; i++){
-		        if(max[i]>result)
-		            result = max[i];
+		        if(max[i]>k)
+		            k = max[i];
 		    }
-		    return result;
+		    return k;
 	}
-	public static void main(String[] args) {
-		int[] seaquence1= {1,0,2,6,3,4,5,11,12,13};
-		int[] seaquence12= {1000000000,22,9,33,21,50,41,60,80,90,90,100,103,108,105};
-		int[] seaquence13= {5,19,5,81,50,28,29,1,83,23};
-		int[]seaquence14 = {1,10,2,4,6,5,13,3,11,7,15};
-		
-		System.out.println(run(seaquence1));
-		System.out.println(run(seaquence12));
-		System.out.println(run(seaquence13));
-		System.out.println(run(seaquence14));
-
-	}
+	
 }
