073fd0958bb9a11331dac93e328d34178ed61346
Erildo Shuli <Erildo@users.noreply.github.com>
2017-10-13 14:39:10 +0200

    Update PrimeP.java

--- PrimeP.java
+++ PrimeP.java
@@ -1,29 +1,31 @@
-import java.awt.List;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class PrimeP {
-	public static void main(String[] args) {
-		int n,p;
-		Scanner s=new Scanner(System.in);
-		System.out.println("Please insert the  number:");
-		n=s.nextInt();
+public class Solution {
+	
+	public static boolean run(int number) {
+		int p=0;
+		boolean isSemiprime = false;
 		ArrayList<Integer> array = new ArrayList<Integer>();
-		for(int i=2;i<n;i++)
-		{  p=0;
-			for(int j=2;j<i;j++)
-			{ 
-				if(i%j==0)
-					p=1;}
-			if(p==0){
-				array.add(i);
-			}}
-		System.out.println(array);
-		for(int i=0; i<array.size(); i++) {
-			for(int j=0; j<array.size(); j++) {
-			if((array.get(i) * array.get(j)) == n )
-			{
-				System.out.println("Is semiprime");
-			}
-			
-			}}}}
+	for(int i=2;i<number;i++)
+	{  p=0;
+		for(int j=2;j<i;j++)
+		{ 
+			if(i%j==0)
+				p=1;}
+		if(p==0){
+			array.add(i);
+		}}
+	for(int i=0; i<array.size(); i++) {
+		for(int j=0; j<array.size(); j++) {
+		if((array.get(i) * array.get(j)) == number )
+		{
+			isSemiprime = true;
+		}
+		}
+
+		}
+	return isSemiprime;
+
+		}}
